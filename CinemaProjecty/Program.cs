78f38155f20b70246906f9462e8493d915824c5c
Robert-Ionut Lumezeanu <robert.lumezeanu@student.upt.ro>
2024-12-31 18:38:37 +0200

    Adaugare CinemaProject

---
+++ CinemaProjecty/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CinemaProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cinematograf");
+        }
+    }
+}
