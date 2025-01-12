aa2b40d1535d5291204e7e7eab523ccf8f21c13a
Alin-Loredan-Ion Mităr <alin.mitar@student.upt.ro>
2025-01-12 23:46:19 +0200

    am facut clasele de utilizator si client ,nu am terminat clasa de client si la functia de creat utilizatori nu stiu ce sa scriu exact cand creez un admin ca nu inteleg in totalitate form-urile :3 <-alin

---
+++ Test_WFA/Utilizator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_WFA
+{
+    class Utilizator
+    {
+        private char _tip;
+        private string _username;
+        private string _parola;
+
+        public char Tip
+        {
+            get
+            {
+                return _tip;
+            }
+            set
+            {
+                _tip = value;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+            }
+        }
+
+        public string Parola
+        {
+            get
+            {
+                return _parola;
+            }
+            set
+            {
+                _parola = value;
+            }
+        }
+
+        public static Utilizator CrUtil(char Tip,String Username,String Parola) 
+        {
+            switch (char.ToUpper(Tip)) 
+            {
+                case 'A':
+                    return new Client(Username, Parola);
+
+                case 'C':
+                    return new Client(Username, Parola);
+
+                default:
+                    throw new ArgumentException("tipul utilizatorului nu este valid!");
+
+            }
+        }
+    }
+}
