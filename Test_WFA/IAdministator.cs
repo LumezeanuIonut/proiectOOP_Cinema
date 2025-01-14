47e47881334f0ac0797fd46684fbc69b02db4ed3
Alin-Loredan-Ion Mităr <alin.mitar@student.upt.ro>
2025-01-15 00:25:16 +0200

    clasa ad,ministrator + interfete client si administrator

---
+++ Test_WFA/IAdministator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_WFA
+{
+    internal interface IAdministrator
+    {
+        void AdaugareFilme(Film film);
+        void StergereFilme(Film film);
+        void IstoricClient(Client client);
+    }
+}
---
+++ Test_WFA/IClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_WFA
+{
+    internal interface IClient
+    {
+        string Cod { get; set; }
+        List<IstoricRezervari> IstoricRezervari { get; set; }
+
+        void CrRezervare(Film film, DateTime InceputRezervaer, DateTime SfarsitRezervare);
+        void ModRezervare(Film film, DateTime InceputNouaRezervare, DateTime SfarsitNouaRezervare);
+        void DelRezervare(Film film, DateTime InceputRezervare, DateTime SfarsitRezervare);
+    }
+}
